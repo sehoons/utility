d26eae5372f7c9a0119abf70d4c3caea70a42aa4
sehoons <seahoon12@gmail.com>
2023-12-10 03:34:17 +0900

    modify

--- utility.js
+++ utility.js
@@ -1,6 +1,13 @@
-let input = document.querySelector("#root > main > div > div > div > div.Trade_trade__aside__Vh1Sl > div > div.SearchContainer_trade-search-container__2N60q > div.search.Search_search--trade__JS55Z > div > div > div").firstChild;
-let nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
-nativeInputValueSetter.call(input, '');
+function getInputValue(cssItem){
+    let nativeInputValueGetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").get;
+    const inputText = nativeInputValueGetter.call(cssItem);
 
-let newEvent = new Event('input', { bubbles: true});
-input.dispatchEvent(newEvent);
+    return inputText;
+}
+
+function setInputValue(cssItem, value){
+    let nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
+    nativeInputValueSetter.call(cssItem, value);
+    let newEvent = new Event('input', { bubbles: true});
+    cssItem.dispatchEvent(newEvent);
+}
