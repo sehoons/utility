42c4ee13b3b09ef2c1c178c83bfac37e78f3cf3f
sehoons <seahoon12@gmail.com>
2024-06-10 03:10:27 +0900

    modify

--- utility.js
+++ utility.js
@@ -1,10 +1,13 @@
+//특정 필드의 값을 가져 옴
 function getInputValue(cssItem){
+    console.log('test')
     let nativeInputValueGetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").get;
     const inputText = nativeInputValueGetter.call(cssItem);
 
     return inputText;
 }
 
+//특정 필드의 값을 강제로 수정함
 function setInputValue(cssItem, value){
     let nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
     nativeInputValueSetter.call(cssItem, value);
